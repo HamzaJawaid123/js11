4e82467700d5064fab4afcf492d40234ecc76bff
Hamza Jawaid <hamzajawaid1001@gmail.com>
2025-09-04 15:36:45 +0500

    Add files via upload
    java.test

---
+++ javascript1/app.js
@@ -0,0 +1,158 @@
+//CHA1
+
+//1
+
+//2
+//   alert("Error! PLease Enter a vailed passward")
+
+
+
+// 3
+// alert("Welcom to JS Land... \n Happy coding!")
+
+
+//  4
+//  alert("Welcom to JS Land...")
+
+//  alert(" Happy coding!")
+
+
+
+// 5
+// alert("Hello... I can run JS through my web browser console")
+
+
+// Que  6,7 Done
+
+
+
+//CH2
+
+
+
+// 1
+// var username =""
+
+
+
+
+//2
+// var myname ="Ubaid"
+
+
+
+
+//3
+// var Titelmeassage= "Hello World"
+//   alert(Titelmeassage)
+
+
+
+//4
+// var a="Jhone Deon"
+// var b=" 15 Year OLd"
+// var c="Cretified Mobile application Development"
+
+// alert(a)
+// alert(b)
+// // alert(c)
+
+
+
+
+
+//5
+//  var A =" PIZZA \n PIZZ \n PIZ \n PI \n P"
+//  alert(A)
+
+
+
+
+
+// 6
+// var a = "My Email Is"
+// var b = "Example@gmail.com"
+// var c = a + b
+// alert(c)
+
+
+
+
+//7
+// var a="I Trying to lEarn from the Book"
+// var b="A smart Way to learn Javascript"
+// alert(  a+b)
+
+
+
+
+
+
+//8
+// document.write("Yah! I can Write HTML Content through Javascript")
+
+
+
+
+//9
+// var g ="The Chapter 2 end"
+// document.write(g)
+// alert(g)
+
+
+
+
+//CHA3
+
+//1
+// var age ="I am 15Years Old"
+// alert(age)
+
+
+//2
+// var a="My Birth year is"
+// var b= 2010;
+// document.write(a +
+// " " +b)
+
+
+//3
+// var a="JHone Doen"
+// var b="Ordered 15 T-shirt(s)"
+// var c="on XYZ CLOTHES store"
+
+// document.write(a+" "+b+" "+c    )
+
+
+//ch4
+
+
+
+
+//1
+// var x, y, z = 1, 2, 3
+
+
+//2
+// legal
+// var myCoun
+// var s_n
+// var X_V
+// var Final_Play
+// var sTore
+// Illegal
+// var 1stvair
+// var m-y
+// var @fff
+// var !ggg
+// var v v
+
+//3
+
+// document.write("Rules for naming JS variables"+"<br>"+
+// "Variable names can only contain letters, digits, underscores (_), 
+// and dollar signs ($).For example: $my_1stVariable"+"<br>"+"Variables must 
+// begin with a letter, underscore (_), or dollar sign ($).For example: $name, _name, name"+"
+// <br>"+"Variable names are case sensitive."+"<br>"+"Variable names should not be JS reserved keywords.")
+
+
